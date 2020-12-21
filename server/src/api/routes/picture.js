1ff06cc326d45ee56b0009ec475d519ec82015ae
Simon Leipold <s.leipold@adito.de>
2020-12-21 15:56:20 +0100

    use adito logo if no user picture found

--- server/src/api/routes/picture.js
+++ server/src/api/routes/picture.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
+const config = require('../../config');
 const aditoService = require('../../services/adito');
+const encodeHelper = require('../../helpers/encodeHelper');
 
 const router = express.Router();
 
@@ -11,7 +13,15 @@ module.exports = (app) => {
     const aditoUserId = req.query.id;
 
     // receives the user profile picture from adito in base64
-    const aditoUserImage = JSON.parse(await aditoService.getUserImage(aditoUserId));
+    let aditoUserImage = JSON.parse(await aditoService.getUserImage(aditoUserId));
+
+    // TODO: replace adito-logo.png with the commonly used user name abbreviation (Tim Admin -> TA)
+    // use default user image if none was found or received
+    if (!aditoUserImage) {
+      aditoUserImage = encodeHelper.base64_encode(
+        `${config.NODE_SERVER_FS_ASSETS_PATH}/images/adito-logo.png`
+      );
+    }
 
     const response = { image: aditoUserImage };
 
