ebcfe2a2b5018291da60224abc90213b3e04234e
Simon Leipold <s.leipold@adito.de>
2020-12-21 17:03:42 +0100

    changed style of chat according to ux design

--- client/src/components/InputIcon/InputIcon.jsx
+++ client/src/components/InputIcon/InputIcon.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import SendIcon from '@material-ui/icons/Send';
 import MicIcon from '@material-ui/icons/Mic';
 
+const iconStyle = { color: "white", background: "#42a5f6", borderRadius: "7px", height: "30px", width: "30px" }
+
 const InputIcon = ({ iconType, isRecording }) => {
   if (iconType === 'text' || isRecording) {
-    return <SendIcon />;
+    return <SendIcon style={iconStyle} />;
   } else {
-    return <MicIcon />;
+    return <MicIcon style={iconStyle} />;
   }
 };
 
