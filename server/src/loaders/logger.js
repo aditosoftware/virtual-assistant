20bf24228070d096c6311bbac91057a312f3b1cc
Simon Leipold <s.leipold@adito.de>
2021-01-13 11:14:40 +0100

    added logging to file (combined.log and error.log)

--- server/src/loaders/logger.js
+++ server/src/loaders/logger.js
@@ -3,8 +3,12 @@ const winston = require('winston');
 const config = require('../config');
 
 const transports = [];
-if (config.NODE_ENV !== 'dev') {
-  transports.push(new winston.transports.Console());
+if (config.NODE_ENV === 'prod') {
+  transports.push(
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  );
 } else {
   transports.push(
     new winston.transports.Console({
